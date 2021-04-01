993f52768bc67dcc27649fb2e364419e15088733
Jabelic <hibiki990523@gmail.com>
2021-04-01 22:30:08 +0900

    add sign-in layout#1

--- pages/sign-in.vue
+++ pages/sign-in.vue
@@ -1,7 +1,12 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <div class="text-center">ほげ！</div>
+      <v-card>
+        <v-card-title> sign-in </v-card-title>
+        <v-card-actions>
+          <v-btn @click="login"> sign-in </v-btn>
+        </v-card-actions>
+      </v-card>
     </v-col>
   </v-row>
 </template>
@@ -9,8 +14,12 @@
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api'
 export default defineComponent({
+  layout: 'auth',
   setup(_, {}) {
-    return {}
+    const login = () => {}
+    return {
+      login,
+    }
   },
 })
 </script>
