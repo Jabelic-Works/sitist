75e6c77ce68ea9294e00b5da96abb7960901c0e7
Jabelic <hibiki990523@gmail.com>
2021-04-06 02:06:34 +0900

    add form and button

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,50 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">ほげ！</div>
-      <h3> hello, {{ userName ? userName : guest }}</h3>
-    </v-col>
-  </v-row>
+  <div class="" justify="center">
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="8" md="6">
+        <div class="text-center">ほげ！</div>
+        <h3>hello, {{ userName ? userName : guest }}</h3>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-form>
+          <v-row>
+            <!-- <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="Name"
+            :rules="nameRules"
+            :counter="10"
+            label="Name"
+            required
+          ></v-text-field>
+        </v-col> -->
+
+            <v-col cols="12" md="4">
+              <v-text-field
+                v-model="submitUrl"
+                :counter="10"
+                label="url"
+                required
+              ></v-text-field>
+            </v-col>
+
+            <!-- <v-col cols="12" md="4">
+              <v-text-field
+                v-model="email"
+                label="title"
+                required
+              ></v-text-field>
+            </v-col> -->
+          </v-row>
+        </v-form>
+        <v-btn>submit</v-btn>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script lang="ts">
@@ -13,16 +53,19 @@ export default defineComponent({
   setup(_, { root }) {
     const guest = 'Guest'
     const userName = ref('')
-
-    onMounted(()=>{
+    const submitUrl = ref('')
+    onMounted(() => {
       console.debug('mounted!!')
       userName.value = root.$store.getters['auth/getUserName']
       console.debug('user name: ', userName.value)
     })
-    watch(()=>root.$store.getters['auth/getUserName'], ()=>{
-      userName.value = root.$store.getters['auth/getUserName']
-    })
-    return { guest, userName }
+    watch(
+      () => root.$store.getters['auth/getUserName'],
+      () => {
+        userName.value = root.$store.getters['auth/getUserName']
+      }
+    )
+    return { guest, userName, submitUrl }
   },
 })
 </script>
