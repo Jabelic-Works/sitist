4e45b1f21e0f399cd841a3a7f902118c32aab4fd
Jabelic <hibiki990523@gmail.com>
2021-04-11 23:27:29 +0900

    add footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,12 +1,40 @@
 <template>
   <div>
-    <footer>footer</footer>
+    <v-footer color="primary lighten-1" padless>
+      <v-row justify="center" no-gutters>
+        <v-btn
+          v-for="link in linksInFooter"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+        >
+          {{ link }}
+        </v-btn>
+        <v-col class="primary lighten-2 py-2 text-center white--text" cols="12">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-col>
+      </v-row>
+    </v-footer>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
 
 export default defineComponent({
-  setup() {},
+  setup() {
+      const  linksInFooter =  [
+        'Home',
+        'About Us',
+        'Team',
+        'Services',
+        'Blog',
+        'Contact Us',
+      ]
+      return {
+          linksInFooter
+      }
+  },
 })
 </script>
