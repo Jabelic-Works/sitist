92f9f10becd24259276489483f0ff36ccc7142e6
Jabelic <hibiki990523@gmail.com>
2021-08-29 03:20:33 +0900

    add: stroe action 'logout'#11

--- middleware/authenticated.ts
+++ middleware/authenticated.ts
@@ -6,7 +6,11 @@ export default ({ store, route, redirect }: any) => {
       store.dispatch('auth/login', user)
       console.debug(store.getters['auth/getUserName'])
     } else {
-      if (route.name !== 'login') redirect('/sign-in')
+      if (route.name !== 'login') {
+        // redirect('/sign-in')
+        console.debug('sign-inに遷移させたい！')
+        // XXX: Redirected when going from "/" to "/sign-in" via a navigation guard.
+      }
     }
   })
 }
