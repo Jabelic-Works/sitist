083968cab2f76b1f47799d997e9b5ed220b9629a
Jabelic <hibiki990523@gmail.com>
2021-08-29 14:07:04 +0900

    add

--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -1,9 +1,15 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import AddInfoDialog from '@/components/AddInfoDialog'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
 
 describe('Logo', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(AddInfoDialog)
     expect(wrapper.vm).toBeTruthy()
+    const wrapperFooter = mount(Footer)
+    expect(wrapperFooter.vm).toBeTruthy()
+    const wrapperHeader = mount(Header)
+    expect(wrapperHeader.vm).toBeTruthy()
   })
 })
