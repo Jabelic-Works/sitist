1e584c8f392f5ffe733c1c74703b2f5aee9e2873
Jabelic <hibiki990523@gmail.com>
2021-11-24 15:42:13 +0900

    FIX: linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,14 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
-    // node: true,
+    node: true,
   },
   extends: [
-    'eslint:recommended',
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
+    "eslint:recommended",
+    "@nuxtjs/eslint-config-typescript",
+    "plugin:prettier/recommended",
+    "plugin:nuxt/recommended",
   ],
   plugins: [],
   // add your custom rules here
