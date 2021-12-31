ef57effa05b15a361bcca1baed9cd2ff902fb81e
Jabelic <hibiki990523@gmail.com>
2021-12-31 21:13:37 +0900

    ADD: setup -> modules

---
+++ modules/Domain/index.ts
@@ -0,0 +1,97 @@
+import { nextTick, onActivated, ref, useContext, useFetch, useStore, watch } from "@nuxtjs/composition-api"
+import { use as useFetchData } from "@/modules/fetchData"
+import { CardInfo } from "~/types/custom"
+import { deepcopy } from "../utils"
+
+export const use = () => {
+  const refUserName = ref("Guest")
+  const refUserUid = ref("")
+  const documentLocalData = ref<{ data: CardInfo }>() // FIXME: type
+  const sitesInfo = ref([])
+  const { store } = useContext()
+  const { fetchAllData } = useFetchData()
+  useFetch(() => {
+    refUserUid.value = store.getters["auth/getUserUid"]
+    if (refUserUid.value) {
+      documentLocalData.value = store.getters["data/getAllData"] // データがある場合
+      console.debug("useFetch", documentLocalData.value)
+      // データがない場合
+      // データを追加していない人だけがサーバーへのアクセスが増える、だめだこれ
+      if (Object.keys(documentLocalData.value).length === 0) {
+        console.debug("data is empty")
+        documentLocalData.value = fetchAllData(refUserUid.value)
+        store.dispatch("data/setAllData", documentLocalData.value)
+      }
+    }
+  })
+  // ユーザーが変わった場合
+  watch(
+    () => store.getters["auth/getUserUid"],
+    () => {
+      if (refUserUid.value !== store.getters["auth/getUserUid"]) {
+        refUserName.value = store.getters["auth/getUserName"]
+        refUserUid.value = store.getters["auth/getUserUid"]
+      }
+    }
+  )
+  // データが更新された場合
+  watch(
+    () => store.getters["data/getAllData"],
+    () => {
+      documentLocalData.value = deepcopy(store.getters["data/getAllData"])
+    }
+  )
+  nextTick(async () => {
+    documentLocalData.value = await deepcopy(store.getters["data/getAllData"])
+    console.debug("nextTick", documentLocalData.value)
+  })
+  onActivated(() => {
+    refUserName.value = store.getters["auth/getUserName"]
+    refUserUid.value = store.getters["auth/getUserUid"]
+    documentLocalData.value = store.getters["data/getAllData"]
+    console.debug("activate", documentLocalData.value)
+    afterPostData()
+  })
+  // 多分storeの更新を待たなきゃいけない, watchではうまく動かない。
+  // stateの更新の完了を検知したいんだけど...
+  /** postした後にstoreの後の値を */
+  const afterPostData = () => {
+    setTimeout(() => checkGetters(), 500)
+  }
+  const afterEditData = () => {
+    setTimeout(() => checkGetters(), 500)
+  }
+  const checkGetters = async () => {
+    documentLocalData.value = await deepcopy(store.getters["data/getAllData"])
+    console.debug(JSON.stringify(documentLocalData.value))
+    let tmpArray = []
+    for (const [key, value] of Object.entries(documentLocalData.value)) {
+      tmpArray.push({ key, data: value.data })
+    }
+    sitesInfo.value = tmpArray
+  }
+  const isShowingUpdateDataDialog = ref(false)
+  const showDialog = () => {
+    isShowingUpdateDataDialog.value = true
+  }
+  const closeDialog = () => {
+    isShowingUpdateDataDialog.value = false
+  }
+  const fData = async () => {
+    await store.dispatch("data/setAllData", fetchAllData(refUserUid.value))
+    setTimeout(() => checkGetters(), 500)
+  }
+  return {
+    refUserName,
+    refUserUid,
+    documentLocalData,
+    afterPostData,
+    checkGetters,
+    fData,
+    isShowingUpdateDataDialog,
+    showDialog,
+    closeDialog,
+    sitesInfo,
+    afterEditData
+  }
+}
--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
       <v-row justify="center" align="center">
         <v-col cols="12" sm="8" md="6">
           <div class="text-center"></div>
-          <h3>hello, {{ refUserName ? refUserName : guest }}</h3>
+          <h3>hello, {{ refUserName }}</h3>
         </v-col>
         <v-col
           ><v-btn @click="checkGetters">updateData: 画面の更新</v-btn>
@@ -29,95 +29,26 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, watch, ref, useContext, useFetch, onActivated, nextTick } from "@nuxtjs/composition-api"
-import { use } from "@/modules/fetchData"
-import { deepcopy } from "~/modules/utils"
-import { CardInfo } from "~/types/custom"
+import { defineComponent } from "@nuxtjs/composition-api"
+import { use } from "@/modules/Domain/index"
 
 export default defineComponent({
   setup() {
-    const guest = "Guest"
-    const refUserName = ref("")
-    const refUserUid = ref("")
-    const documentLocalData = ref<{ data: CardInfo }>() // FIXME: type
-    const sitesInfo = ref([])
-    const { store } = useContext()
-    const { fetchAllData } = use()
-    // すでにstoreにデータがある場合は再取得はボタンで行う(回数制限/有料制にする？)
-    // const { $fetch } =
-    useFetch(() => {
-      refUserUid.value = store.getters["auth/getUserUid"]
-      if (refUserUid.value) {
-        documentLocalData.value = store.getters["data/getAllData"] // データがある場合
-        console.debug("useFetch", documentLocalData.value)
-        // データがない場合
-        // データを追加していない人だけがサーバーへのアクセスが増える、だめだこれ
-        if (Object.keys(documentLocalData.value).length === 0) {
-          console.debug("data is empty")
-          documentLocalData.value = fetchAllData(refUserUid.value)
-          store.dispatch("data/setAllData", documentLocalData.value)
-        }
-      }
-    })
-    // ユーザーが変わった場合
-    watch(
-      () => store.getters["auth/getUserUid"],
-      () => {
-        if (refUserUid.value !== store.getters["auth/getUserUid"]) {
-          refUserName.value = store.getters["auth/getUserName"]
-          refUserUid.value = store.getters["auth/getUserUid"]
-        }
-      }
-    )
-    // データが更新された場合
-    watch(
-      () => store.getters["data/getAllData"],
-      () => {
-        documentLocalData.value = deepcopy(store.getters["data/getAllData"])
-      }
-    )
-    nextTick(async () => {
-      documentLocalData.value = await deepcopy(store.getters["data/getAllData"])
-      console.debug("nextTick", documentLocalData.value)
-    })
-    onActivated(() => {
-      refUserName.value = store.getters["auth/getUserName"]
-      refUserUid.value = store.getters["auth/getUserUid"]
-      documentLocalData.value = store.getters["data/getAllData"]
-      console.debug("activate", documentLocalData.value)
-      afterPostData()
-    })
-    // 多分storeの更新を待たなきゃいけない, watchではうまく動かない。
-    // stateの更新の完了を検知したいんだけど...
-    /** postした後にstoreの後の値を */
-    const afterPostData = () => {
-      setTimeout(() => checkGetters(), 500)
-    }
-    const afterEditData = () => {
-      setTimeout(() => checkGetters(), 500)
-    }
-    const checkGetters = async () => {
-      documentLocalData.value = await deepcopy(store.getters["data/getAllData"])
-      console.debug(JSON.stringify(documentLocalData.value))
-      let tmpArray = []
-      for (const [key, value] of Object.entries(documentLocalData.value)) {
-        tmpArray.push({ key, data: value.data })
-      }
-      sitesInfo.value = tmpArray
-    }
-    const isShowingUpdateDataDialog = ref(false)
-    const showDialog = () => {
-      isShowingUpdateDataDialog.value = true
-    }
-    const closeDialog = () => {
-      isShowingUpdateDataDialog.value = false
-    }
-    const fData = async () => {
-      await store.dispatch("data/setAllData", fetchAllData(refUserUid.value))
-      setTimeout(() => checkGetters(), 500)
-    }
+    const {
+      refUserName,
+      refUserUid,
+      documentLocalData,
+      afterPostData,
+      checkGetters,
+      fData,
+      isShowingUpdateDataDialog,
+      showDialog,
+      closeDialog,
+      sitesInfo,
+      afterEditData
+    } = use()
+
     return {
-      guest,
       refUserName,
       refUserUid,
       documentLocalData,
