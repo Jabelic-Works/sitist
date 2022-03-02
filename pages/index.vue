26caf885af4434ee48f75f91ee98ebe71056e712
Jabelic <hibiki990523@gmail.com>
2022-03-03 00:15:11 +0900

    HOTFIX: template index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,60 +1,18 @@
 <template>
   <div>
-    <Header>
-      <template slot="AddInfoDialog">
-        <AddInfoDialog
-          :isShowAddInfodialog="isShowAddInfodialog"
-          :refUserName="refUserName"
-          :refUserUid="refUserUid"
-          :update="afterPostData"
-          @unshowAddInfodialog="unshowAddInfodialog"
-        />
-      </template>
-    </Header>
-    <v-container>
-      <div class="" justify="center">
-        <v-row justify="center" align="center">
-          <v-col cols="12" sm="8" md="6">
-            <div class="text-center"></div>
-            <h3>hello, {{ refUserName }}</h3>
-          </v-col>
-          <v-col
-            ><v-btn @click="checkGetters">updateData: 画面の更新</v-btn>
-            <v-btn @click="showDialog">fetchAllData: データの更新</v-btn>
-          </v-col>
-        </v-row>
-        <v-row v-if="documentLocalData">
-          <v-col v-for="doc in sitesInfo" :key="doc.key" cols="20">
-            <CardComponent :cardInfo="doc" @afterPostData="afterPostData" @afterEditData="afterEditData" />
-          </v-col>
-        </v-row>
-      </div>
-      <ConfirmDialog
-        :is-opened="isShowingUpdateDataDialog"
-        @closeDialog="closeDialog"
-        @fechData="fData"
-        :confirmText="'データを更新しますか？'"
-      />
-      <AddInfoDialog
-        :kinds="'floating'"
-        :isShowAddInfodialog="isShowAddInfodialog"
-        :refUserName="refUserName"
-        :refUserUid="refUserUid"
-        :update="afterPostData"
-        @unshowAddInfodialog="unshowAddInfodialog"
-      />
-    </v-container>
+    <CardList />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "@nuxtjs/composition-api"
+import CardList from "~/templates/CardList.vue"
 
 export default defineComponent({
   setup() {
     return {}
-  }
-
+  },
+  components: { CardList }
 })
 </script>
 <style scoped>
