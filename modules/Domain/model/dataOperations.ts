1af2bb8e8fa634bc5190f928bef7162848dc2a19
Jabelic <hibiki990523@gmail.com>
2022-04-03 00:04:29 +0900

    FIX: move.

--- modules/Domain/model/dataOperations.ts
+++ modules/Domain/model/dataOperations.ts
@@ -1,5 +1,5 @@
 import { Context } from "@nuxt/types"
-import { CardInfo, SiteInformation } from "~/types/custom"
+import { CardInfo } from "~/types/custom"
 import { deleteData } from "../../firestoreClient/deleteData"
 import { commitEditedCardInfo } from "../../firestoreClient/editData"
 
@@ -16,16 +16,6 @@ export const deleteCardInformation = async (info: CardInfo, store) => {
     }
   }
   store.dispatch("data/setAllData", storeData)
-}
-
-const isSiteInformation = (test: any): test is SiteInformation => {
-  return (
-    test.data &&
-    typeof test.data.title == "string" &&
-    typeof test.data.URL == "string" &&
-    (typeof test.data.OGP == "string" || typeof test.data.OGP == undefined) &&
-    (typeof test.data.description == "string" || typeof test.data.description == undefined)
-  )
 }
 
 export const editCardInfomation = async (info: CardInfo, store: Context["store"]) => {
