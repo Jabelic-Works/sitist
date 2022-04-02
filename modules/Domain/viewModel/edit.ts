eada1def531c83513da1b8d32e29b9025e43cdd0
Jabelic <hibiki990523@gmail.com>
2022-04-03 02:17:05 +0900

    TWEAK: typo

--- modules/Domain/viewModel/edit.ts
+++ modules/Domain/viewModel/edit.ts
@@ -3,7 +3,7 @@ import { useStore } from "@nuxtjs/composition-api"
 import { CardInfo } from "~/types/custom"
 import { commitEditedCardInfo } from "../../firestoreClient/editData"
 
-export const editCardInfomation = async (info: CardInfo, store: Context["store"]) => {
+export const editCardInformation = async (info: CardInfo, store: Context["store"]) => {
   await commitEditedCardInfo(info, store, info.key)
   store.dispatch("data/setData", { data: info.data, id: info.key })
 }
