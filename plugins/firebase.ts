b2ddd3792cf6353c6c6fcb82bfa683ec04349aee
Jabelic <hibiki990523@gmail.com>
2022-04-09 10:29:29 +0900

    FIX

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -1,16 +1,17 @@
 import firebase from "firebase"
 import "firebase/storage"
 // ** Firebaseプロジェクトの設定を記す
+export const firebaseConfig = {
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_AUTHDOMAIN,
+  projectId: process.env.FB_PROJECT_ID,
+  storageBucket: process.env.FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.FB_MESSAGEING_SENDER_ID,
+  appId: process.env.FB_APP_ID,
+  measurementId: process.env.FB_MEASUREMENT_ID
+}
 if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: process.env.FB_API_KEY,
-    authDomain: process.env.FB_AUTHDOMAIN,
-    projectId: process.env.FB_PROJECT_ID,
-    storageBucket: process.env.FB_STORAGE_BUCKET,
-    messagingSenderId: process.env.FB_MESSAGEING_SENDER_ID,
-    appId: process.env.FB_APP_ID,
-    measurementId: process.env.FB_MEASUREMENT_ID
-  })
+  firebase.initializeApp(firebaseConfig)
 }
 
 export const db = firebase.firestore()
